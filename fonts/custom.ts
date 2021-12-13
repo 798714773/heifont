c6e5f2442c0e859efffb26f05ddece289b08bdee
798714773 <798714773@qq.com>
2021-12-13 21:02:24 +0800

    add fonts

---
+++ fonts/custom.ts
@@ -0,0 +1,10 @@
+// [VexFlow](https://vexflow.com) - Copyright (c) Mohit Muthanna 2010.
+// MIT License
+
+import { CustomFont } from './custom_glyphs';
+import { CustomMetrics } from './custom_metrics';
+
+export const Custom = {
+  data: CustomFont,
+  metrics: CustomMetrics,
+};
---
+++ fonts/gonville.ts
@@ -0,0 +1,10 @@
+// [VexFlow](https://vexflow.com) - Copyright (c) Mohit Muthanna 2010.
+// MIT License
+
+import { GonvilleFont } from './gonville_glyphs';
+import { GonvilleMetrics } from './gonville_metrics';
+
+export const Gonville = {
+  data: GonvilleFont,
+  metrics: GonvilleMetrics,
+};
